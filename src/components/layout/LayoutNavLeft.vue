a3faa4d1bf0f000c8c46f99a61e9b99c39d6d82d
riohp <carloshenao0418@gmail.com>
2024-08-09 17:21:51 -0500

    m

---
+++ src/components/layout/LayoutNavLeft.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="wrapper">
+      <side-bar>
+        <sidebar-link to="/admin/table-list">
+          <i class="nc-icon nc-notes"></i>
+          <p>Lista de agentes</p>
+        </sidebar-link>
+        <sidebar-link to="/admin/table-list">
+          <i class="nc-icon nc-notes"></i>
+          <p>Lista cancelados</p>
+        </sidebar-link>
+      </side-bar>
+     
+    </div>
+  </template>
+  <style lang="scss">
+  
+  </style>
+  <script>
+    import DashboardContent from './Content.vue'
+    export default {
+      components: {
+          DashboardContent,
+      },
+      methods: {
+        toggleSidebar () {
+          if (this.$sidebar.showSidebar) {
+            this.$sidebar.displaySidebar(false)
+          }
+        }
+      }
+    }
+  
+  </script>
+  
