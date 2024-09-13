ae597e76f64a36a92fe244939367837934126a49
SANTIAGO VERGARA FRANCO <BB9260@d03.abaigroup.global>
2024-09-13 16:56:08 -0500

    Modified login with nice alerts

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,11 +1,25 @@
 <template>
-  <main class="mt-0 main-content">
+  <main class="mt-0 main-content no-text-select">
     <section>
       <div class="page-header min-vh-100">
         <div class="container">
           <div class="row">
-            <div class="mx-auto col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0">
-              <div class="card card-plain">
+            <!-- Bloque de imagen a la izquierda -->
+            <div class="col-lg-6 d-flex justify-content-center flex-column">
+              <div
+                class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden background-image"
+              >
+                <span class="mask bg-gradient-dark opacity-6"></span>
+                <h4 class="mt-5 text-white font-weight-bolder position-relative">
+                  Control de Agenda ABAI
+                </h4>
+                <p class="text-white position-relative">
+                  Sistema diseñado para el registro y control de sus referidos.
+                </p>
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center">
+              <div class="card card-plain w-100">
                 <div class="pb-0 card-header text-start">
                   <h4 class="font-weight-bolder">Inicio de Sesión</h4>
                   <p class="mb-0">Ingrese su usuario y contraseña</p>
@@ -22,17 +36,20 @@
                         size="lg"
                       />
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 position-relative password-container">
                       <argon-input
                         v-model="password"
                         id="password"
-                        type="password"
+                        :type="showPassword ? 'text' : 'password'"
                         placeholder="Contraseña"
                         name="password"
                         size="lg"
                       />
+                      <div class="password-toggle" @click.stop="togglePassword">
+                        <i :class="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
+                      </div>
                     </div>
-                    <div v-if="errorMessage" class="text-center text-danger mb-3">
+                    <div v-if="errorMessage" class="alert alert-danger text-white" role="alert">
                       {{ errorMessage }}
                     </div>
                     <div class="text-center">
@@ -50,23 +67,6 @@
                     </div>
                   </form>
                 </div>
-              </div>
-            </div>
-            <div class="top-0 my-auto text-center col-6 d-lg-flex d-none h-100 pe-0 position-absolute end-0 justify-content-center flex-column">
-              <div
-                class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
-                style="
-                  background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
-                  background-size: cover;
-                "
-              >
-                <span class="mask bg-gradient-dark opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">
-                  Control de Agenda ABAI
-                </h4>
-                <p class="text-white position-relative">
-                  Sistema diseñado para el registro y control de sus referidos.
-                </p>
               </div>
             </div>
           </div>
@@ -90,6 +90,7 @@ const userName = ref('');
 const password = ref('');
 const errorMessage = ref('');
 const isLoading = ref(false);
+const showPassword = ref(false);
 
 const body = document.getElementsByTagName("body")[0];
 
@@ -108,6 +109,10 @@ onBeforeUnmount(() => {
   store.state.showFooter = true;
   body.classList.add("bg-gray-100");
 });
+
+const togglePassword = () => {
+  showPassword.value = !showPassword.value;
+};
 
 const handleLogin = async () => {
   errorMessage.value = '';
@@ -137,3 +142,67 @@ const handleLogin = async () => {
   }
 };
 </script>
+
+<style scoped>
+.background-image {
+  background-image: url('@/assets/img/test.jpg');
+  background-size: cover;
+  background-position: center;
+  min-height: 500px;
+}
+
+.alert {
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 1rem;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+}
+
+.alert-danger {
+  color: #721c24;
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+}
+
+.password-container {
+  position: relative;
+}
+
+.password-toggle {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  color: #adb5bd;
+  z-index: 10;
+  padding: 5px;
+}
+
+.password-toggle:hover {
+  color: #6c757d;
+}
+
+.no-text-select {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.no-text-select * {
+  cursor: default;
+}
+
+.no-text-select input,
+.no-text-select textarea,
+.no-text-select [contenteditable] {
+  cursor: text;
+  user-select: text;
+}
+
+.no-text-select .password-toggle,
+.no-text-select button {
+  cursor: pointer;
+}
+</style>
